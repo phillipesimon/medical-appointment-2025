73b7a1b7789f4c3a864f1b6db21ac13cbd8f35f0
Simon <phillipe.simon@gmail.com>
2025-10-23 09:47:40 -0300

    Creating the User Authentication Controller

---
+++ src/models/users/useCases/authenticate-user/authenticate-user.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { IPasswordCrypto } from "../../../../infra/shared/crypto/password.crypto";
+import { IUserRepository } from "../../repositories/user.repository";
+import { AuthenticateUserUseCase } from "./authenticate-user.usercase";
+
+export class AuthenticateUserController {
+  constructor(
+    private userRepository: IUserRepository,
+    private passwordCrypto: IPasswordCrypto
+  ) {}
+
+  async handle(request: Request, response: Response) {
+    try {
+      const data = request.body;
+
+      const authenticateUserUseCase = new AuthenticateUserUseCase(
+        this.userRepository,
+        this.passwordCrypto
+      );
+
+      const result = await authenticateUserUseCase.execute(data);
+
+      return response.json(result);
+    } catch (err: any) {
+      return response.status(err.statusCode).json({
+        error: err.message,
+      });
+    }
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { createUserController } from "../models/users/useCases/create-user";
+import { authenticateUserController } from "../models/users/useCases/authenticate-user";
 
 const userRouter = Router();
+
+userRouter.post("/login", async (req, res) => {
+  await authenticateUserController.handle(req, res);
+});
 
 userRouter.post("/users", async (req, res) => {
   await createUserController.handle(req, res);
