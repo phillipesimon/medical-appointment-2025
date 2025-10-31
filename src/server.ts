f86df9b7aaf4382597cea34db24ac2255989e4a6
Simon <phillipe.simon@gmail.com>
2025-10-31 12:11:24 -0300

    Creating a doctor controller

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { userRouter } from "./routes/user.routes";
 import { specialityRouter } from "./routes/speciality.routes";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
+import { doctorRouter } from "./routes/doctor.routes";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(userRouter);
 app.use(specialityRouter);
+app.use(doctorRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
