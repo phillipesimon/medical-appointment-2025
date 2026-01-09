928a511275548d69e15fba587e460ba95ceb771d
Simon <phillipe.simon@gmail.com>
2026-01-09 04:16:04 -0300

    Upload files with Multer

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,7 +1,22 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { createPatientController } from "../modules/patient/useCases/create-patient";
 
+import multer from "multer";
+
+const uploadTest = multer({
+  dest: "./upload",
+});
+
 const patientRouter = Router();
+
+patientRouter.post(
+  "/upload-test",
+  uploadTest.single("fileTest"),
+  (request: Request, response: Response) => {
+    console.log(request.file);
+    return response.json({ message: "Ok" });
+  }
+);
 
 patientRouter.post("/patients", async (request, response) => {
   await createPatientController.handle(request, response);
